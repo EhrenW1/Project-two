f078ff5a4ed1ef9d882614536795a5f44ad6141c
EhrenW1 <ehren.wargo@patelhs.org>
2020-10-14 12:52:01 +0000

    Added an if else statment that says weather to go outside or stay inside based on time and temperature

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 class Main {
   public static void main(String[] args) {
- int time = 1400
+ int time = 1400;
  int tempurature = 80;
  boolean bikeing = true; 
  boolean sunny = false;
  boolean rainy = true;
- boolean tired = false
- boolean walking = false
-  }
-}
+ boolean tired = false;
+ boolean walking = false;
+ // Inserted variables for later
+ // these variables include time, tenourature, if its sunny,rainy, and if your walking or biking.
+  if (time < 1800 && tempurature < 90)
+   //made if statment for if the temp is below 90 and the time is before 6 oclock 
+   {
+     System.out.print(" Go out side.");
+    // if the variables match conditions it prints go out side 
+
+   }else{
+     System.out.print(" Stay inside.");
