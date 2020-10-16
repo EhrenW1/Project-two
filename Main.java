97156cdc2b7facfe19bcc3b7785c53291bf0ddfd
EhrenW1 <ehren.wargo@patelhs.org>
2020-10-16 03:16:06 +0000

    Changed the name from walking to exercising because it made more sense

--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ class Main {
  boolean tired = false;
  boolean walking = false;
  int dayNumber = 15;
- int dayNumberLastWalked = 14;
- int timeFeelLikeWalking = 7;
+ int dayNumberLastExercised = 14;
+ int timeFeelLikeExercising = 7;
  // Inserted variables for later
  // these variables include time, tenourature, if its sunny,rainy, and if your walking or biking.
       if (tempurature < 90 && sunny == true)
@@ -19,7 +19,7 @@ class Main {
      System.out.print("Go out side and");
      //wrote line of code that tells you to go outside if its sunny and below 90
    }else{
-     System.out.print("Stay inside to walk and");
+     System.out.print("Stay inside to exersise and");
      // else stament that tells you to stay inside if if statment values dont match
         }
     if (fealLikeBikeing == true || fealLikeSkateing == true )
@@ -30,16 +30,16 @@ class Main {
      System.out.print(" dont grab anything.");
      // else stament that tells you not to grab anything
         }
-        System.out.print(" You should walk for ");
-              while (timeFeelLikeWalking < 20)
+        System.out.print(" You should exercise for ");
+              while (timeFeelLikeExercising < 20)
 {
-   timeFeelLikeWalking++; 
+   timeFeelLikeExercising++; 
 }
-System.out.print(timeFeelLikeWalking + " minutes. ");
-     if ( dayNumberLastWalked != dayNumber - 1  )
+System.out.print(timeFeelLikeExercising + " minutes. ");
+     if ( dayNumberLastExercised != dayNumber - 1  )
    //made if statment that tells you to walk for an extra 10 minutes if you didnt walk yesterday.
      {
-     System.out.print(" However, since you didnt walk yesterday Walk for an extra 10 minutes.");
+     System.out.print(" However, since you didnt exercise yesterday Walk for an extra 10 minutes.");
      }else{
     System.out.println("");
      // else stament that prints nothing if you did exercise yesterday
